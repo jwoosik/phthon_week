33f801c9fc9506224abd1d8c8c578cc0790c90a7
jwoosic <a88925600@gmail.com>
2024-02-24 13:12:51 +0900

    2024-02-24

--- 2024-02-24.py
+++ 2024-02-24.py
@@ -1,7 +1,5 @@
-print("test")
-
-
-print("test")
-# alt + shift + f
-# ctrl + shift + p => 기능들을 나열하는 단축키
-# ctrl + , 
+'''
+alt + shift + f
+ctrl + shift + p => 기능들을 나열하는 단축키
+ctrl + , => 설정 검색
+'''
